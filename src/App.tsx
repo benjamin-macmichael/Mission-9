9659b35b60cf19f00f66fa11597d591f2d1c5908
Ben Macmichael <benjaminmac21@gmail.com>
2024-03-07 10:29:21 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import teamsData from "./CollegeBasketballTeams.json";
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Welcome() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        College Teams in NCAA Basketball
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>School: {oneTeam.school}</h1>
+          <h3>Mascot: {oneTeam.name}</h3>
+          <h3>City: {oneTeam.city}</h3>
+          <h3>State: {oneTeam.state}</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamsData.teams.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
